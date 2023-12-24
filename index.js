750a9504e5d0d8dd5c073c1b183038f1ed9f13b4
noodelz <adrianeroa081202@gmail.com>
2023-12-24 15:29:26 +0800

    configured env

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import { router } from './api.js'
-
-const corsConfig = {
-    origin: 'http://localhost:3000'
-}
-
 const app = express()
 const PORT = process.env.PORT
+
+const corsConfig = {
+    origin: process.env.CLIENT_URL
+}
 
 app.use(cors(corsConfig))
 app.use(bodyParser.json())
